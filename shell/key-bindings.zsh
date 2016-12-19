b6b0077e40824a911e0e37d73cb783278ea7d8a7
Liu Xiang <liuxiang@ktjr.com>
2016-12-19 10:10:01 +0800

    Use sed instead of gsed

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -65,7 +65,7 @@ zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
 
 fzf-autojump-widget() {
-  LBUFFER="${LBUFFER}$(autojump -s | gsed -n '/^_______/!p; /^_______/q'  | cut -d$'\t' -f2 | fzf | sed "s#^#'#;s#\$#'#")"
+  LBUFFER="${LBUFFER}$(autojump -s | sed -n '/^_______/!p; /^_______/q'  | cut -d$'\t' -f2 | fzf | sed "s#^#'#;s#\$#'#")"
   zle redisplay
 }
 zle     -N   fzf-autojump-widget
